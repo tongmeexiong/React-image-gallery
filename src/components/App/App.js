abc511beea01d260f0f2ae1445f24451bb235e00
Tongmee Xiong <tongmeexiong@gmail.comlspwd>
2019-05-03 16:16:26 -0500

    added get on app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
+
 
 class App extends Component {
+
+componentDidMount(){
+  this.addPhoto();
+}
+  state = {
+    image: []
+  }
+
+
+  addPhoto = () => {
+    axios.get('/gallery')
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          image: response.data
+        })
+      }).catch((err) => {
+        console.log('Error in GET', err);
+      })
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -10,7 +34,7 @@ class App extends Component {
         </header>
         <br/>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {/* <img src="images/goat_small.jpg"/> */}
       </div>
     );
   }
