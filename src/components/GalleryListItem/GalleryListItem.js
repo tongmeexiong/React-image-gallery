7edb1153d416855825f8c5cc72ba422e041843fb
Tongmee Xiong <tongmeexiong@gmail.comlspwd>
2019-05-04 18:11:13 -0500

    added description change

--- src/components/GalleryListItem/GalleryListItem.js
+++ src/components/GalleryListItem/GalleryListItem.js
@@ -4,37 +4,36 @@ import React from 'react';
 class GalleryListItem extends React.Component {
 
     state = {
-        imageClick: false
+        imageClicked: false
     }
 
     handleClick = () => {
-        console.log('CLICKED');
         this.setState({
-            imageClick: !this.props.imageClick
+            imageClicked: !this.state.imageClicked
         })
     }
-
 
     render() {
         console.log(this.state);
         let photoToChange;
-        if (this.state.imageClick) {
-            photoToChange = (
+        if (this.state.imageClicked) {
+            photoToChange = 
                 <div>
-                    <ol onClick={this.handleClick}><img src={this.props.image.path} /></ol>
-                    <button onClick={() => this.props.likes(this.props.image.id)}> LIKE</button>
-                    {this.props.image.likes}
-                </div>
-            )
-        }
-        else {
-            photoToChange = (
-                <div>
-                    <ol onClick={this.handleClick}> {this.props.image.description}</ol>
+                <ol onClick={this.handleClick}> {this.props.image.description}</ol>
                     <button onClick={() => this.props.likes(this.props.image.id)}> LIKE</button>
                     {this.props.image.likes}
                 </div >
-            )
+               
+        }
+        else {
+            photoToChange = 
+                <div>
+
+                <ol><img onClick={this.handleClick} src={this.props.image.path} /></ol>
+                    <button onClick={() => this.props.likes(this.props.image.id)}> LIKE</button>
+                    {this.props.image.likes}
+                </div>
+            
         }
         return (
             photoToChange
