3baddacee614055d3de48b64e2a97aed4b94ce27
Tongmee Xiong <tongmeexiong@gmail.comlspwd>
2019-05-03 20:55:53 -0500

    added change of image with description in listitems

--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -7,7 +7,6 @@ class GalleryList extends React.Component {
             <ul>
                 {this.props.imageList.map((image) => {
                     return (
-
                             <GalleryListItem key={image.id} image={image} likes={this.props.buttonLikes}/>
                    )
                 })}
--- src/components/GalleryListItem/GalleryListItem.js
+++ src/components/GalleryListItem/GalleryListItem.js
@@ -1,16 +1,43 @@
 import React from 'react';
 
 
-class GalleryListItem extends React.Component{
+class GalleryListItem extends React.Component {
 
-    render(){
-        return(
-            <div>
-                <img src={this.props.image.path} /> 
-                <button onClick={()=>this.props.likes(this.props.image.id)}> LIKE</button>
-                {this.props.image.description}
-                {this.props.image.likes}
-            </div>
+    state = {
+        imageClick: false
+    }
+
+    handleClick = () => {
+        console.log('CLICKED');
+        this.setState({
+            imageClick: !this.props.imageClick
+        })
+    }
+
+
+    render() {
+        console.log(this.state);
+        let photoToChange;
+        if (this.state.imageClick) {
+            photoToChange = (
+                <div>
+                    <ol onClick={this.handleClick}><img src={this.props.image.path} /></ol>
+                    <button onClick={() => this.props.likes(this.props.image.id)}> LIKE</button>
+                    {this.props.image.likes}
+                </div>
+            )
+        }
+        else {
+            photoToChange = (
+                <div>
+                    <ol onClick={this.handleClick}> {this.props.image.description}</ol>
+                    <button onClick={() => this.props.likes(this.props.image.id)}> LIKE</button>
+                    {this.props.image.likes}
+                </div >
+            )
+        }
+        return (
+            photoToChange
         )
     }
 }
