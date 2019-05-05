7c61d80b7662592c54eb1bdad6f4c5698e18f522
Tongmee Xiong <tongmeexiong@gmail.comlspwd>
2019-05-05 14:20:36 -0500

    fixed description

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -4,10 +4,10 @@ const galleryItems = [
     { id: 3, path: 'images/zipline.jpg', description: 'Ziped Lined from one island to another in the Philipines! This was a wild adventure. Almost got mugged, got lost in a village of non english speakers and almost missed my flight!', likes: 0 },
     { id: 4, path: 'images/firstcliff.jpg', description: 'My first Cliff jump, My first time swimming in the deep end and My first time realizing how lucky I truly am to still be alive!', likes: 0 },
     { id: 5, path: 'images/canada.jpg', description: 'Hiking over 7,000ft elevation in Banff Alberta, Canada. Happened to be National Canada Day that very same day.', likes: 0 },
-    { id: 6, path: 'images/dive.jpg', description: 'Fun fact: I can free dive up to 30ft , holding one breathe', likes: 0 },
+    { id: 6, path: 'images/dive.jpg', description: 'Fun fact: I can free dive up to 30ft, holding one breathe.', likes: 0 },
     { id: 7, path: 'images/railroad.jpg', description: 'I breakdanced for over 10 years. I competed and performed all over Minnesota. I can still breakdance but not as I used to when I was younger and more healthy.', likes: 0 },
     { id: 8, path: 'images/climb.jpg', description: 'This day I hiked down a mountain and climbed 5 waterfalls with no harnesses. First one was 70ft high and they continually got shorter.', likes: 0 },
-    { id: 9, path: 'images/jits.jpg', description: 'I love JiiJitsu. It is a grappling martial arts that is very popular in MMA. I started doing it at a low point in my life and it helped me overcome a great depression! I still do it today and I love the friends I have made from it.', likes: 0 },
+    { id: 9, path: 'images/jits.jpg', description: 'I love JiuJitsu. It is a grappling martial arts that is very popular in MMA. I started doing it at a low point in my life and it helped me overcome a great depression! I still do it today and I love the friends I have made from it.', likes: 0 },
 
 ];
 module.exports = galleryItems;
